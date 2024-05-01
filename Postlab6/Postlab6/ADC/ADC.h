a2d8d87e9211ed5cda5b76953cd12f7fd15bdaf0
Snakefra <158300295+Snakefra@users.noreply.github.com>
2024-05-01 14:31:33 -0600

    Create ADC.h

---
+++ Postlab6/Postlab6/ADC/ADC.h
@@ -0,0 +1,19 @@
+/*
+ * ADC.h
+ *
+ * Created: 30/04/2024 09:01:51
+ *  Author: Snake
+ */ 
+
+
+#ifndef ADC_H_
+#define ADC_H_
+
+
+#include <stdint.h>
+
+void initADC(void);
+uint16_t readADC(void);
+
+
+#endif /* ADC_H_ */
